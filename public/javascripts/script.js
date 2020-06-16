c985613dd16c4e2844fa461598857f743fb0ce5d
Dennis Yeung <dennis@maic.biz>
2020-06-16 13:39:15 +0800

    change websocker URL

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,6 @@
 //var socket = io.connect(`http://localhost:${process.env.SOCKET_PORT}`);
-var socket = io.connect('http://localhost:8010');
+//var socket = io.connect('http://localhost:8010');
+var socket = io.connect('http://safe-badlands-36894.herokuapp.com:8010');
 
 // Add dynamic html bot content(Widget style) ----------------------------
 // You can also add the html content in html page and still it will work!
